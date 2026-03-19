a45d1f9572180d13d37c9bffe053e76b194f666b
Ezequiel Martin <ezequielmartin92@gmail.com>
2026-03-19 18:17:59 -0300

    Implemento el GenreService, Implemento el GenreMapper, Agrego metodo necesario al Repository e Implemento todos los DTOs de Genre

---
+++ src/main/java/com/ezequielmartin/horrorverse_api/dto/GenreResponseDTO.java
@@ -0,0 +1,18 @@
+package com.ezequielmartin.horrorverse_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreResponseDTO {
+    private Long id;
+    private String genreName;
+    private List<MovieOfGenreDTO> movies;
+}
--- src/main/java/com/ezequielmartin/horrorverse_api/repository/IGenreRepository.java
+++ src/main/java/com/ezequielmartin/horrorverse_api/repository/IGenreRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IGenreRepository extends JpaRepository<Genre,Long> {
+    boolean existsByNameIgnoreCase(String name);
 }
